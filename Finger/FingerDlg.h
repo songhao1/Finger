f9339de936088e3a8f22cc40e083012b0ee02148
717977831 <717977831@qq.com>
2020-06-15 01:34:39 +0800

    first

---
+++ Finger/FingerDlg.h
@@ -0,0 +1,71 @@
+﻿
+// FingerDlg.h: 头文件
+//
+
+#pragma once
+
+#include "CZKFPEngX.h"//引入指纹采集器SDK
+
+
+#include<direct.h>//for _mkdir()
+#define TEMP_DIR "temp\\"//临时文件夹，用于存放指纹入库前的所有中间处理结果
+#define DB_DIR "Database\\"//指纹库文件夹
+#define DB_INDEX_TXT "Database\\index.txt"//指纹库索引文件
+void InitDatabase();//创建并初始化指纹库
+void InitFile(char *filename);//创建并初始化文件
+void GetDatabaseInfo(char *info);//获取指纹库统计信息
+
+
+// CFingerDlg 对话框
+class CFingerDlg : public CDialogEx
+{
+// 构造
+public:
+	CFingerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FINGER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+	DECLARE_EVENTSINK_MAP()
+	afx_msg void OnImageReceivedZkfpengx(BOOL FAR* AImageValid);
+public:
+	CStatic m_staticInfo;
+	CStatic m_picCtrl1;
+	CStatic m_picCtrl2;
+	CString m_name;
+	int m_minuCount;
+	CZKFPEngX m_zkfpEng;
+	afx_msg void OnBnClickedBtnExit();
+	afx_msg void OnBnClickedBtnConnect();
+	afx_msg void OnBnClickedBtnDisconnect();
+	afx_msg void OnBnClickedBtnCapture();
+	afx_msg void OnBnClickedBtnStep1();
+	afx_msg void OnBnClickedBtnDatabase();
+	afx_msg void OnBnClickedBtnStep2();
+	afx_msg void OnBnClickedBtnStep3();
+	afx_msg void OnBnClickedBtnStep4();
+	afx_msg void OnBnClickedBtnStep5();
+	afx_msg void OnBnClickedBtnStep6();
+	afx_msg void OnBnClickedBtnStep7();
+	afx_msg void OnBnClickedBtnStep8();
+	afx_msg void OnBnClickedBtnStep9();
+	afx_msg void OnBnClickedBtnStep10();
+	afx_msg void OnBnClickedBtnStep11();
+	afx_msg void OnBnClickedBtnStep12a();
+	afx_msg void OnBnClickedBtnStep12b();
+};
---
+++ Finger/resource.h
@@ -0,0 +1,44 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Finger.rc 使用
+//
+#define IDD_FINGER_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_STEP_1                  1000
+#define IDC_BTN_STEP_2                  1001
+#define IDC_BTN_STEP_3                  1002
+#define IDC_BTN_STEP_4                  1003
+#define IDC_BTN_STEP_5                  1004
+#define IDC_BTN_STEP_6                  1005
+#define IDC_BTN_STEP_7                  1006
+#define IDC_BTN_STEP_8                  1007
+#define IDC_BTN_STEP_9                  1008
+#define IDC_BTN_STEP_10                 1009
+#define IDC_BTN_STEP_11                 1010
+#define IDC_BTN_STEP_12A                1011
+#define IDC_BTN_STEP_12B                1012
+#define IDC_BTN_ADD_IMAGE               1013
+#define IDC_BTN_MATCH_IMAGE             1014
+#define IDC_BTN_CONNECT                 1015
+#define IDC_BTN_CAPTURE                 1016
+#define IDC_BTN_REGISTER                1017
+#define IDC_BTN_IDENTIFY                1018
+#define IDC_BTN_DISCONNECT              1019
+#define IDC_BTN_DATABASE                1020
+#define IDC_BTN_EXIT                    1021
+#define IDC_STATIC_INFO                 1022
+#define IDC_STATIC_IMG_1                1023
+#define IDC_STATIC_IMG_2                1024
+#define IDC_EDIT_NAME                   1025
+#define IDC_ZKFPENGX                    1026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
